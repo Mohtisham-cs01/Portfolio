11458b716f3bc276a5e6757cdee54b04af473d9a
Mohtisham-cs01 <117306878+Mohtisham-cs01@users.noreply.github.com>
2026-08-21 14:33:42 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../assets/Footer.css';
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2024 Alex Chen. Designed with ❤️ and lots of ☕</p>
+      <p>&copy; 2024 Muhtasham. Designed with ❤️ and lots of ☕</p>
     </footer>
   );
 };
